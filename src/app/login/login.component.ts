b68b686d032215d370d64eb5116a7374de8a398e
Towhidul Bashar <towhidulbashar@users.noreply.github.com>
2019-08-23 13:22:52 +0600

    Show login failed message. Redirected to registration page(temporary) after successful login.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { UserLogin } from './login.model';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginService } from './login.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -9,21 +11,26 @@ import { LoginService } from './login.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginFailed: boolean = false;
   loginFormGroup: FormGroup = this.fb.group({
     userName: ['', [Validators.required]],
     password: ['', [Validators.required]]
   });
   constructor(private fb: FormBuilder,
-    private loginService: LoginService) {
+    private loginService: LoginService, 
+    private router: Router) {
    }
   login(userLogin: UserLogin){
-    console.log('userlogin: ', userLogin);
     this.loginService.login(userLogin)
       .subscribe(result => {
-        console.log('login result: ', result);
+        if (result.status == 200) 
+          this.router.navigate(['/registration']);
+        else
+          this.loginFailed = true;
       },
       error => {
         console.log('login error: ', error);
+        this.loginFailed = true;
       });
   }
 }
